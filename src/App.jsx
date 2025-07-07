51ba4194ce641a24c33c0dfe0f493283268f37a2
Your Name <your.email@example.com>
2025-07-07 19:23:27 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Hero from './components/hero';
 import Navigation from './components/Navigation';
 import SkillSection from './components/SkillSection';
-import AboutSection from './components/Aboutsection';
+// import AboutSection from './components/AboutSection';
 import Contact from './components/Contact';
 import ProjectSection from './components/ProjectSection';
 import Footer from './components/footer';
+import AboutSection from './components/AboutSection';
 
 const App = () => {
   return (
